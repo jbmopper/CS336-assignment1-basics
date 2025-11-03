84c5a3e8e227ec0787cc2317540ab64cf5c44196
jbmopper <jbmopper@gmail.com>
2025-11-03 15:28:30 -0700

    added swiglu

--- cs336_basics/implementations.py
+++ cs336_basics/implementations.py
@@ -1,6 +1,10 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-__all__ = ['linear', 'embeddings']
+
+
+__all__ = ['linear', 'embeddings', "SwiGLU"]
 
 def linear(weights, in_features):
     return torch.matmul(in_features, weights.T)
@@ -8,3 +12,16 @@ def linear(weights, in_features):
 def embeddings(weights, token_ids):
     return weights[token_ids]
 
+class SwiGLU(nn.Module):
+    
+    def __init__(self, d_model, d_ff) -> None:
+        super().__init__()
+        self.w1 = nn.Linear(d_model, d_ff, bias=False)
+        self.w2 = nn.Linear(d_ff, d_model, bias=False)
+        self.w3 = nn.Linear(d_model, d_ff, bias=False)
+        # w1_weight, w2_weight, w3_weight
+        # get added by the calling function...
+        self.swish = nn.SiLU()
+
+    def forward(self, x):
+        return self.w2(self.swish(self.w1(x))*self.w3(x))
--- tests/adapters.py
+++ tests/adapters.py
@@ -87,7 +87,14 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+    # raise NotImplementedError
+    swiglu = SwiGLU(d_model, d_ff)
+    swiglu.w1.weight.data = w1_weight
+    swiglu.w2.weight.data = w2_weight
+    swiglu.w3.weight.data = w3_weight 
+
+    return swiglu.forward(in_features)
+
 
 
 def run_scaled_dot_product_attention(
