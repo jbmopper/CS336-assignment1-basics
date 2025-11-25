8572a31e3b80e9fd27a33ffc059354e5b4849f5a
jbmopper <jbmopper@gmail.com>
2025-11-25 12:29:49 -0700

    bpe trainer for testing

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -47,36 +47,20 @@ def train_bpe(input_path: str | os.PathLike,
 
     # first pass    
     for pretoken, b in tokenized.items():
-        if len(b) < 2:
-            continue 
-        for i in range(len(b)-1): 
+        for i in range(len(b)-1): # skips len 1 b's
             paircount[(b[i], b[i+1])] += 1 * pretoken_counts[pretoken]
 
-    
-    
-    merge = max(paircount.items(), key=lambda kv: (kv[1], kv[0]))[0] # bot says this does it all...
-    merges.append(merge)
-    vocab[len(vocab)] = bytes(merge)
 
     while len(vocab) < vocab_size:
-        update = next(reversed(vocab.values()))
-        for pretoken, count in pretokens.items():
-            splits = pretoken.encode("utf-8").split(update)
-            # splits will be a list.  1 element means no split (or all the token)
-            # "b''" elements indicate beginning/end/multiple-in-a-row
-            if len(splits) == 1:
-                continue
-            elif splits[0] == b'':
-                match = check_vocab(vocab, splits[1], "l")
-                # update counts
-            elif splits[-1] == b'':
-                # uhh need to check further into the sequence in case there's multiple b''...
-                # no, that's just "update update"
-                
-            for i in len(splits):
+        # need the byte gumming here
+        merge = max(paircount.items(), key=lambda kv: (kv[1], kv[0]))[0] # bot says this does it all...
+        merges.append(merge)
+        vocab[len(vocab)] = bytes(merge) # combines into one bytes object
+        update_tokens(merge, tokenized)
+
+    return vocab, merges
 
 
-            
 
 def pretokenize(
     file: BinaryIO,
@@ -92,32 +76,30 @@ def pretokenize(
     return pretoken_counts
 
 
-    
+def update_tokens( # updates token lists and pair counts
+    merge: tuple[bytes, bytes],
+    tokenized: dict[str, [bytes]],
+    paircount: Counter,
+    pretoken_count: Counter
+) -> None:
+    for pretoken, b in tokenized.items():
+        updated_tokens = []
+        i = 0
+        while i < len(b):
+            if i < len(b) - 1 and b[i] == merge[0] and b[i+i] == merge[1]:
+                updated_tokens.append(bytes(merge))
+                if i > 0:
+                    paircount[(b[i-1], merge[0])] -= pretoken_count[pretoken] 
+                if i + 2 < len(b):
+                    paircount[(merge[1], b[i+2])] -= pretoken_count[pretoken]
+                i += 2
+            else:
+                updated_tokens.append(b[i])
+        tokenized[pretoken] = updated_tokens
 
-
-        
-def check_vocab(vocab: dict[int, bytes], split: bytes, side) -> bytes:
-    if side is "l": # tries to match left side of the adjacent bytes
-        substring = split
-        tester = (substring in vocab.values())
-        while tester is False:
-            substring = substring[:-1]
-            tester = (substring in vocab.values())
-        return substring
-    elif side is "r": # tries to match the right side of the adjacent bytes
-        substring = split
-        tester = (substring in vocab.values())
-        while tester is False:
-            substring = substring[1:]
-            tester = (substring in vocab.values())
-        return substring
-    else:
-        return b""
-
-
-
-
-
+    # remove the merged pair from paircount
+    del(paircount[merge])
+    return None
 
 
 
