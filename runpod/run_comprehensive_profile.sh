4a3032ef3da56d8f73faabf461edd93a952f06e7
jbmopper <jbmopper@gmail.com>
2026-02-03 19:08:16 -0700

    hopefully will work (not horribly slow)

--- runpod/run_comprehensive_profile.sh
+++ runpod/run_comprehensive_profile.sh
@@ -17,12 +17,22 @@ OUTPUT_BASE="profile_results/comprehensive"
 mkdir -p "$OUTPUT_BASE"
 
 # Array of profiling scenarios (format: "name args...")
+# Configurations validated to fit in 24GB VRAM with memory analysis
+# See: notebooks/gpu_model_analysis.py for memory calculations
 CONFIGS=(
-    "assignment      --model assignment"
-    "bandwidth_bound --batch-size 256 --d-model 384 --num-heads 6 --num-layers 4 --d-ff 1024"
-    "compute_bound   --batch-size 32 --d-model 1536 --num-heads 24 --num-layers 8 --d-ff 4096"
-    "attn_memory     --batch-size 16 --d-model 512 --num-heads 8 --num-layers 6 --seq-len 2048"
-    "vocab_bound     --batch-size 64 --d-model 512 --num-heads 8 --num-layers 4 --vocab-size 50257"
+    # Baseline Models (from configs/models.yaml)
+    "model_a           --batch-size 32 --seq-len 256 --d-model 768 --num-heads 12 --num-layers 2 --d-ff 2048"
+    "model_b           --batch-size 32 --seq-len 256 --d-model 384 --num-heads 12 --num-layers 12 --d-ff 1024"
+    
+    # Bottleneck & Stress Tests (sorted by memory usage)
+    "latency_bound     --batch-size 1 --seq-len 128 --d-model 512 --num-heads 8 --num-layers 12 --d-ff 1536"
+    "misaligned_dims   --batch-size 33 --seq-len 256 --d-model 513 --num-heads 9 --num-layers 6 --d-ff 1537"
+    "bad_head_size     --batch-size 32 --seq-len 256 --d-model 672 --num-heads 12 --num-layers 6 --d-ff 1792"
+    "vocab_bottleneck  --batch-size 64 --seq-len 256 --d-model 512 --num-heads 8 --num-layers 4 --d-ff 1536 --vocab-size 50257"
+    "wide_ffn          --batch-size 32 --seq-len 256 --d-model 768 --num-heads 12 --num-layers 6 --d-ff 4096"
+    "bandwidth_bound   --batch-size 256 --seq-len 256 --d-model 384 --num-heads 6 --num-layers 4 --d-ff 1024"
+    "compute_bound     --batch-size 32 --seq-len 256 --d-model 1536 --num-heads 24 --num-layers 8 --d-ff 4096"
+    "deep_sequential   --batch-size 32 --seq-len 256 --d-model 512 --num-heads 8 --num-layers 32 --d-ff 1536"
 )
 
 NSYS_AVAILABLE=1
@@ -119,6 +129,7 @@ run_phase_1() {
                     --data-dir "$DATA_DIR" \
                     --precision bf16 \
                     --steps 20 \
+                    --warmup 10 \
                     "${WARGS[@]}" \
                     $ARGS
         else
@@ -129,6 +140,7 @@ run_phase_1() {
                 --data-dir "$DATA_DIR" \
                 --precision bf16 \
                 --steps 20 \
+                --warmup 10 \
                 "${WARGS[@]}" \
                 $ARGS
         fi
@@ -159,9 +171,9 @@ run_phase_2() {
         fi
         
         if [[ $NCU_AVAILABLE -eq 1 ]]; then
-            # Run ncu profile (1 step only!)
+            # Run ncu profile (1 step only with 10 warmup iterations!)
             CUDA_VISIBLE_DEVICES=0 run_cmd "$NAME" "$OUT_DIR" ncu \
-                --set full \
+                --set detailed \
                 --target-processes all \
                 -o "$OUT_DIR/analysis" \
                 --force-overwrite \
@@ -171,7 +183,7 @@ run_phase_2() {
                     --data-dir "$DATA_DIR" \
                     --precision bf16 \
                     --steps 1 \
-                    --warmup 2 \
+                    --warmup 10 \
                     "${WARGS[@]}" \
                     $ARGS
         else
