0a4a7394ebd5b921f1b7aee16389f2188fedc2a2
jbmopper <jbmopper@gmail.com>
2026-03-09 15:00:59 -0600

    updating onnx for kv cache

---
+++ lambda-onnx-inference/run_onnx_local.py
@@ -0,0 +1,14 @@
+import numpy
+import onnxruntime as rt
+
+session = rt.InferenceSession("./export/models/baseline_best.onnx", providers=["CPUExecutionProvider"])
+
+print("onnx inputs")
+for inp in session.get_inputs():
+    print(inp.name, inp.shape, inp.type)
+
+print("onnx outputs")
+
+for outp in session.get_outputs():
+    print(outp.name, outp.shape, outp.type)
+
