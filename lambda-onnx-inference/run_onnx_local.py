a38ed6acd36fd331d4d376af0a4af78b8cde12f7
jbmopper <jbmopper@gmail.com>
2026-03-10 11:29:08 -0600

    local onnx script

--- lambda-onnx-inference/run_onnx_local.py
+++ lambda-onnx-inference/run_onnx_local.py
@@ -1,13 +1,55 @@
 import numpy
 import onnxruntime as rt
+import argparse
+import tokenizer as t
 
-session = rt.InferenceSession("./export/models/baseline_best.onnx", providers=["CPUExecutionProvider"])
 
-print("onnx inputs")
-for inp in session.get_inputs():
-    print(inp.name, inp.shape, inp.type)
 
-print("onnx outputs")
-for outp in session.get_outputs():
-    print(outp.name, outp.shape, outp.type)
+ARTIFACT_BASEPATH = "./export/"
+MODEL_BASEPATH = "models/"
+TOKENIZER_BASEPATH = "tokenizer/"
+PREFILL_SUFFIX = "_prefill_best.onnx"
+DECODE_SUFFIX = "_decode_best.onnx"
+MODEL_NAME = "baseline"
 
+PREFILL_PATH = ARTIFACT_BASEPATH + MODEL_BASEPATH + MODEL_NAME + PREFILL_SUFFIX
+DECODE_PATH = ARTIFACT_BASEPATH + MODEL_BASEPATH + MODEL_NAME + DECODE_SUFFIX
+TOKENIZER_PATH = ARTIFACT_BASEPATH + TOKENIZER_BASEPATH
+
+SPECIAL_TOKENS = ["<|endoftext|>"]
+PROMPT = " "
+
+
+def _print_snapshot_io(prefill, decode):
+    print("prefill inputs")
+    for inp in prefill.get_inputs():
+        print(inp.name, inp.shape, inp.type)
+
+    print("prefill outputs")
+    for outp in prefill.get_outputs():
+        print(outp.name, outp.shape, outp.type)
+
+    print("decode inputs")
+    for inp in decode.get_inputs():
+        print(inp.name, inp.shape, inp.type)
+
+    print("decode outputs")
+    for outp in decode.get_outputs():
+        print(outp.name, outp.shape, outp.type)
+
+def _setup_tokenizer(path, special_tokens):
+    vocab, merges = t.load_bpe(path)
+    tokenizer = t.Tokenizer(vocab, merges, special_tokens)
+    return tokenizer
+
+def main():
+    tokenizer = _setup_tokenizer(TOKENIZER_PATH, SPECIAL_TOKENS)
+    prefill = rt.InferenceSession(PREFILL_PATH, providers=["CPUExecutionProvider"])
+    decode = rt.InferenceSession(DECODE_PATH, providers=["CPUExecutionProvider"])
+    _print_snapshot_io(prefill, decode)
+    
+
+
+
+if __name__ == "__main__":
+    main()
